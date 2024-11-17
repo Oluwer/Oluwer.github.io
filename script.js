6fa7f76d5e6b72e36fa548672f5866e32c2ecd07
Oliwier <89788541+Oluwer@users.noreply.github.com>
2024-11-17 17:58:12 +0100

    bhjgbhjkfchbaKLJSdfnhlik

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function PlaySound(path)
+
+var audioElem = new Audio();
+  audioElem.src = 'assets/music.ogg';
+  audioElem.loop = true;
+
+function PlaySound()
 {
 
-  var audioElem = new Audio();
-  audioElem.src = path;
-  audioElem.loop = true;
   audioElem.play();
   
 }
